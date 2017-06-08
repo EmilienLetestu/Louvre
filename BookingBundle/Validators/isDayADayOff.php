94e9341da03de7339e2d1dd4c8c6f93991fdece7
emilien_letestu <eletestu@gmail.com>
2017-06-08 10:31:59 +0200

    [add]create a new custom constraint => check if day is weekly closing day

---
+++ BookingBundle/Validators/isDayADayOff.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 08/06/2017
+ * Time: 09:58
+ */
+
+namespace EL\BookingBundle\Validators;
+
+use Symfony\Component\Validator\Constraint;
+
+class isDayADayOff extends Constraint
+{
+    /**
+     * @var string
+     */
+  public $message = "Le musée est fermé tous les mardis, veuillez choisir une autre date !";
+
+  public function validatedBy()
+  {
+     return get_class($this).'Validator';
+  }
+}
---
+++ BookingBundle/Validators/isDayADayOffValidator.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 08/06/2017
+ * Time: 10:06
+ */
+
+namespace EL\BookingBundle\Validators;
+
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Constraint;
+
+class isDayADayOffValidator extends ConstraintValidator
+{
+    public function validate($value,Constraint $constraint)
+    {
+        $day_off = 'Tue';
+        $day = date('D',$value->getTimeStamp());
+
+       if($day === $day_off)
+       {
+           $this->context->buildViolation($constraint->message)
+                         ->addViolation();
+       }
+    }
+}
