56cb951bff10b8b6494566ff13e08d71585f1eab
emilien_letestu <eletestu@gmail.com>
2017-06-30 09:50:19 +0200

    [add] now genrate a qrcode

--- BookingBundle/Services/Mail.php
+++ BookingBundle/Services/Mail.php
@@ -12,6 +12,8 @@ namespace EL\BookingBundle\Services;
 use Doctrine\ORM\EntityManager;
 use EL\BookingBundle\Managers\Tools;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\QrCode;
 
 class Mail
 {
@@ -38,7 +40,6 @@ class Mail
      */
     public function sendMail($email)
     {
-        $tools = new Tools();
         $billing = $this->session->get('billing');
         //get user tickets
         $repository  = $this->doctrine->getRepository('ELBookingBundle:Ticket');
@@ -58,11 +59,13 @@ class Mail
             $ticket->getPriceType();
             $ticket->getTimeAccessType();
         }
+        $this->generateQrCode($order_token,$billing->getId());
         $visit_day = $billing->getVisitDay()->format('d-m-Y');
         //create mail
         $message = \Swift_Message::newInstance();
-        $logo    = $message->embed(\Swift_Image::fromPath('../web/logo_pyramide_accueil.png'));
-        $thumbnail = $message->embed(\Swift_Image::fromPath('../web/louvre_thumbnail.jpg'));
+        $code = 'qrcode'.$billing->getId().'.png';
+        $logo    = $message->embed(\Swift_Image::fromPath('../web/images/logo_pyramide_accueil.png'));
+        $qr_code = $message->embed(\Swift_Image::fromPath('../web/Qrcodes/'.$code));
         $message
             ->setSubject('Votre commande de billets d\'entrée au Musée du Louvre')
             ->setFrom('billetterie_louvre@gmail.com')
@@ -72,12 +75,26 @@ class Mail
                 'visit_day'   => $visit_day,
                 'ticket_list' => $ticket_list,
                 'image_logo'  => $logo,
-                'banner'  => $thumbnail
+                'qr_code'     => $qr_code
             ]),'text/html');
 
         //send mail
         $this->mailer->send($message);
         //will be used later on to detect end of process and kill session
         $this->session->set('mail_sent',1);
+    }
+
+    public function generateQrCode($order_token,$billing_id)
+    {
+        $qr_code = new QrCode();
+        $qr_code
+            ->setText($order_token)
+            ->setErrorCorrectionLevel(ErrorCorrectionLevel::HIGH)
+            ->setLabel('Présenter ce code à l\'accueil')
+            ->setLogoPath('../web/images/logo_pyramide_accueil.png')
+        ;
+        $code = 'qrcode'.$billing_id.'.png';
+        $qr_code->writeFile('../web/Qrcodes/'.$code);
+
     }
 }
